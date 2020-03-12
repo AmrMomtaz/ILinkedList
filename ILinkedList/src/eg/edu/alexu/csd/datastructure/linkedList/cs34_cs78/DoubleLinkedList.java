083a82a7b4001fff88743037bccb131414f0ade4
Amr Momtaz <Amr Momtaz@MOMTAZ>
2020-03-12 18:27:19 +0200

    Double Linked lists + tests

---
+++ ILinkedList/src/eg/edu/alexu/csd/datastructure/linkedList/cs34_cs78/DoubleLinkedList.java
@@ -0,0 +1,154 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs34_cs78;
+
+public class DoubleLinkedList implements ILinkedList
+{
+	private class DNode
+	{
+		Object element;
+		DNode next , prev;
+		private DNode(Object e, DNode p , DNode n)
+		{
+			element = e;
+			next = n;
+			prev = p;
+		}
+		Object get_element() { return element;}
+		DNode get_prev() {return prev;}
+		DNode get_next() {return next;}
+		void set_element(Object e) {element = e;}
+		void set_previous (DNode new_prev) {prev = new_prev;}
+		void set_next (DNode new_next) {next = new_next;}
+	}
+	DNode header,trailer;
+	int size;
+	DoubleLinkedList ()
+	{
+		header = new DNode(null,null,null);
+		trailer  = new DNode(null,header,null);
+		header.set_next(trailer);
+	}
+
+	public void add(int index, Object element)
+	{
+		if (index == size)
+		{
+			add(element);
+			return;
+		}
+		if (index>size)throw new IllegalArgumentException("The index is out of bounds");
+		int i = 0;
+		DNode w = header.get_next();
+		while (i<=index&&w!=trailer)
+		{
+			if(index ==i)
+			{
+				DNode new_node = new DNode(element,w.get_prev(),w);
+				w.get_prev().set_next(new_node);
+				w.set_previous(new_node);
+				size++;
+				return;
+			}
+			i++;
+			w = w.get_next();
+		}
+	}
+
+	public void add(Object element)
+	{
+		DNode w = trailer.get_prev();
+		DNode new_node = new DNode(element,w,trailer);
+		w.set_next(new_node);
+		trailer.set_previous(new_node);
+		size++;
+	}
+
+	public Object get(int index)
+	{
+		if (index >=size)throw new IllegalArgumentException("The index is out of bounds");
+		DNode w = header.get_next();
+		for (int i = 0; i <size;i++)
+		{
+			if (i == index)return w.get_element();
+			w = w.get_next();
+		}
+		return null;
+	}
+
+	public void set(int index, Object element)
+	{
+		if (index >=size)throw new IllegalArgumentException("The index is out of bounds");
+		DNode w = header.get_next();
+		for(int i = 0 ; i<=index ;i++)
+		{
+			if (i == index)	w.set_element(element);
+			w = w.get_next();
+		}
+	}
+
+	public void clear()
+	{
+		header.set_next(trailer);
+		trailer.set_previous(header);
+	}
+
+	public boolean isEmpty()
+	{
+		return (header.next==trailer);
+	}
+
+	public void remove(int index)
+	{
+		if (index >=size)throw new IllegalArgumentException("The index is out of bounds");
+		DNode w = header.get_next();
+		for (int i = 0 ; i <= index ;i++)
+		{
+			if (i == index)
+			{
+				w.get_prev().set_next(w.get_next());
+				w.get_next().set_previous(w.get_prev());
+				w = new DNode(null,null,null);
+				size--;
+			}
+			w = w.get_next();
+		}
+	}
+	
+	public int size()
+	{
+		return size;
+	}
+	
+	/**
+	* @param fromIndex
+	* @param toIndex
+	* @return a view of the portion of this list between the specified
+	* fromIndex and toIndex, inclusively.
+	*/
+	public ILinkedList sublist(int fromIndex, int toIndex)
+	{
+		return null;
+	}
+	
+	public boolean contains(Object o)
+	{
+		if (isEmpty())return false;
+		for (int i = 0;i<size;i++)
+		{
+			Object ob = get(i);
+			if(ob == o)return true;
+		}
+		return false;
+	}
+	
+	public String DDebug()
+	{
+		String result = "[";
+		for (int i = 0;i<size;i++)
+		{
+			result+=get(i).toString();
+			if(i<size-1)result+=",";
+		}
+		result+="]";
+		return result;
+	}
+}
---
+++ ILinkedList/src/eg/edu/alexu/csd/datastructure/linkedList/cs34_cs78/DoubleLinkedListTests.java
@@ -0,0 +1,65 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs34_cs78;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DoubleLinkedListTests 
+{
+
+	@Test
+	void test1() 
+	{
+		DoubleLinkedList test = new DoubleLinkedList();
+		assertEquals(test.contains(5), false);
+		test.add(0);
+		test.add(1);
+		test.add(2);
+		test.add(3);
+		String actual = test.DDebug();
+		String expected = "[0,1,2,3]";
+		assertEquals(expected, actual);
+		assertEquals(true,test.contains(3));
+		assertEquals(false,test.contains(5));
+		test.clear();
+		assertEquals(true, test.isEmpty());
+	}
+	@Test
+	void test2()
+	{
+		DoubleLinkedList test = new DoubleLinkedList();
+		test.add(0);
+		test.add(1);
+		test.add(2);
+		test.add(3);
+		test.remove(2);
+		test.add(2,99);
+		test.add(0,98);
+		test.add(0,4);
+		String actual = test.DDebug();
+		String expected = "[4,98,0,1,99,3]";
+		assertEquals(expected, actual);
+		assertEquals(true,test.contains(3));
+		assertEquals(false,test.contains(5));
+	}
+	@Test
+	void test3()
+	{
+		DoubleLinkedList test = new DoubleLinkedList();
+		assertEquals(true,test.isEmpty());
+		test.add(0,3);
+		test.add(0,2);
+		test.add(0,1);
+		test.add(0,0);
+		test.add(4,4);
+		String actual = test.DDebug();
+		String expected = "[0,1,2,3,4]";
+		assertEquals(expected, actual);
+		Exception IllegalArgumentException = assertThrows(IllegalArgumentException.class, () -> 
+	    {
+	    	test.get(50);
+	    });
+	    String expectedMessage = "The index is out of bounds";
+	    String actualMessage = IllegalArgumentException.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
+	}
+}
